b1e761529fde9969fef104ac0315ea01f4107115
Stefan Koch <Uplink.Coder@googlemail.com>
2016-06-25 21:19:47 +0200

    move fastMath into it's own file and fix compilation with gdc

--- source/bf_compiler.d
+++ source/bf_compiler.d
@@ -15,63 +15,20 @@ string indentBy(const string line, const uint iLvl) {
 		indent[i] = '\t';
 	}
 
-	return cast(string)(indent) ~ line;
+	return cast(string)(indent ~ line);
 }
 
-const(uint) fastLog10(const uint val) pure nothrow @nogc {
-	return 
-	(val < 10) ? 0 : 
-	(val < 100) ? 1 : 
-	(val < 1000) ? 2 : 
-	(val < 10000) ? 3 :
-	(val < 100000) ? 4 :
-	(val < 1000000) ? 5 :
-	(val < 10000000) ? 6 :
-	(val < 100000000) ? 7 :
-	(val < 1000000000) ? 8 : 9;
-}
 
-const(uint) fastPow10(const uint val) pure nothrow {
-	switch(val) {
-		case 0 : return 1;
-		case 1 : return 10;
-		case 2 : return 100;
-		case 3 : return 1000;
-		case 4 : return 10000;
-		case 5 : return 100000;
-		case 6 : return 1000000;
-		case 7 : return 10000000;
-		case 8 : return 100000000;
-		case 9 : return 1000000000;
-		default : assert(0, "value 10 ^ '" ~ (val).itos ~ "' is too big to fit into an uint");
-	}
-}
 
-const(uint) fastPow10tbl(const uint val) pure nothrow {
-	static immutable tbl = [
-		1,
-		10,
-		100,
-		1000,
-		10000,
-		100000,
-		1000000,
-		10000000,
-		100000000,
-		1000000000,
-	] ;
-	if (val < 10) {
-		return tbl[val];
-	} else {
-		assert(0, "value 10 ^ '" ~ (val).itos ~ "' is too big to fit into an uint");
-	}
-}
 /**
- * It returns a string to a mutable buffer.
- * Meaning if you keep 
- *  fiber_safe
+ * It returns a slice from a mutable buffer.
+ * Meaning if you keep thr return value around,
+ * it will change after a second call to itos
+ * This could also have funny effects when uses with fibers.
  */ 
-ref const(string) itos(const uint val) pure {
+import bf_fastMath;
+
+string itos(const uint val) pure {
 	const length = cast(uint)fastLog10(val) + 1;
 	static const char[10] _result;
 	char[] result;
@@ -82,7 +39,7 @@ ref const(string) itos(const uint val) pure {
 		result = cast(char[10]) _result;
 	}
 	foreach(i;0 .. length) {
-		const _val = val / fastPow10(i);
+		immutable _val = val / fastPow10(i);
 		result[length-i-1] = cast(char) ((_val % 10) + '0'); 
 	}
 
---
+++ source/bf_fastmath.d
@@ -0,0 +1,35 @@
+﻿module bf_fastMath;
+
+const(uint) fastLog10(const uint val) pure nothrow @nogc {
+	return 
+		(val < 10) ? 0 : 
+		(val < 100) ? 1 : 
+		(val < 1000) ? 2 : 
+		(val < 10000) ? 3 :
+		(val < 100000) ? 4 :
+		(val < 1000000) ? 5 :
+		(val < 10000000) ? 6 :
+		(val < 100000000) ? 7 :
+		(val < 1000000000) ? 8 : 9;
+}
+
+/*@unique*/ static immutable fastPow10tbl = [
+	1,
+	10,
+	100,
+	1000,
+	10000,
+	100000,
+	1000000,
+	10000000,
+	100000000,
+	1000000000,
+] ;
+
+const(uint) fastPow10(const uint val) pure nothrow @nogc {
+	if (val < 10) {
+		return fastPow10tbl[val];
+	} else {
+		assert(0, "values bigger then 10 ^ 9 are too big to fit into an uint");
+	}
+}
