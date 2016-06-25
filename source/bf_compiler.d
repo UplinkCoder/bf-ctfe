0cba6c3cce8066e426270cb4b51c34998a3269c6
Stefan Koch <Uplink.Coder@googlemail.com>
2016-06-25 18:40:05 +0200

    get rid of std.conv.to and thereby improve CT-performance by A LOT!

--- source/bf_compiler.d
+++ source/bf_compiler.d
@@ -18,9 +18,33 @@ string indentBy(const string line, const uint iLvl) {
 	return cast(string)(indent) ~ line;
 }
 
+const(uint) fastLog10(const uint val) pure nothrow @nogc {
+	return (val < 10) ? 0 : 
+	(val < 100) ? 1 : 
+	(val < 1000) ? 2 : 
+	(val < 10000) ? 3 :
+	(val < 100000) ? 4 :
+	(val < 1000000) ? 5 :
+	(val < 10000000) ? 6 :
+	(val < 100000000) ? 7 :
+	(val < 1000000000) ? 8 : 9;
+}
+
+
+const(string) itos(const uint val) pure {
+	import std.math : log10;
+	const length = cast(uint)fastLog10(val) + 1;
+	char[] result = new char[](length);
+	foreach(i;0 .. length) {
+		const _val = val / (10 ^^ i);
+		result[length-i-1] = cast(char) ((_val % 10) + '0'); 
+	}
+
+	return cast(const(string)) result;
+}
+
 string genCode(const RepeatedToken[] programm, const TargetEnum target, const uint cellSize = 4096) {
-	import std.conv : to;
-	string result = `((const ubyte[] input){uint iPos; ubyte[] output; ubyte[` ~ cellSize.to!string ~ `] cells; ubyte* thisPtr = cells.ptr;` ~ "\n\n";
+	string result = `((const ubyte[] input){uint iPos; ubyte[] output; ubyte[` ~ cellSize.itos ~ `] cells; ubyte* thisPtr = cells.ptr;` ~ "\n\n";
 	uint iLvl = 1;
 
 
@@ -35,17 +59,17 @@ string genCode(const RepeatedToken[] programm, const TargetEnum target, const ui
 				} break;
 
 				case IncVal : {
-					result ~= "(*thisPtr) += ".indentBy(iLvl) ~ rt.count.to!string ~" ;\n";
+					result ~= "(*thisPtr) += ".indentBy(iLvl) ~ rt.count.itos ~" ;\n";
 				} break;
 				case DecVal : {
-					result ~= "(*thisPtr) -= ".indentBy(iLvl) ~ rt.count.to!string ~" ;\n";
+					result ~= "(*thisPtr) -= ".indentBy(iLvl) ~ rt.count.itos ~" ;\n";
 				} break;
 
 				case IncPtr : {
-					result ~= "thisPtr += ".indentBy(iLvl) ~ rt.count.to!string ~" ;\n";
+					result ~= "thisPtr += ".indentBy(iLvl) ~ rt.count.itos ~" ;\n";
 				} break;
 				case DecPtr : {
-					result ~= "thisPtr -= ".indentBy(iLvl) ~ rt.count.to!string ~" ;\n";
+					result ~= "thisPtr -= ".indentBy(iLvl) ~ rt.count.itos ~" ;\n";
 				} break;
 
 				case InputVal : {
@@ -66,5 +90,5 @@ string genCode(const RepeatedToken[] programm, const TargetEnum target, const ui
 		return result ~ "\nreturn output;})";
 	}
 
-	assert(0, "Target Not supported: " ~ to!string(target));
+	assert(0, "Target Not supported: " ~ itos(target));
 }
