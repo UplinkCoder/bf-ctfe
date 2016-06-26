214cd8872ec46a34ff6bc61c8252c9d0ec47604f
UplinkCoder <uplink.coder@googlemail.com>
2016-06-26 03:10:02 +0200

    make the output a static array ... this makes the asm much more readable

--- source/bf_compiler.d
+++ source/bf_compiler.d
@@ -4,10 +4,9 @@ import bf_parser;
 
 enum TargetEnum {
 	AnonymousFunction,
-
 }
 
-string indentBy(const string line, const uint iLvl) {
+string indentBy(const string line, const uint iLvl) pure {
 	char[] indent;
 	indent.length = iLvl;
 
@@ -20,16 +19,16 @@ string indentBy(const string line, const uint iLvl) {
 
 
 
-/**
- * It returns a slice from a mutable buffer.
- * Meaning if you keep thr return value around,
- * it will change after a second call to itos
- * This could also have funny effects when uses with fibers.
- */ 
 import bf_fastMath;
 
+/**
+ * It returns a slice from a mutable buffer.
+ * Meaning if you keep the return value around,
+ * it will change after a second call to itos
+ * This could also have funny effects when used with fibers.
+ */ 
 string itos(const uint val) pure {
-	const length = cast(uint)fastLog10(val) + 1;
+	immutable length = fastLog10(val) + 1;
 	static const char[10] _result;
 	char[] result;
 
@@ -38,6 +37,7 @@ string itos(const uint val) pure {
 	} else {
 		result = cast(char[10]) _result;
 	}
+	
 	foreach(i;0 .. length) {
 		immutable _val = val / fastPow10(i);
 		result[length-i-1] = cast(char) ((_val % 10) + '0'); 
@@ -48,8 +48,15 @@ string itos(const uint val) pure {
 
 static assert(mixin(uint.max.itos) == uint.max);
 
-string genCode(const RepeatedToken[] programm, const TargetEnum target, const uint cellSize = 4096) {
-	string result = `((const ubyte[] input){uint iPos; ubyte[] output; ubyte[` ~ cellSize.itos ~ `] cells; ubyte* thisPtr = cells.ptr;` ~ "\n\n";
+string genCode(const RepeatedToken[] programm, const TargetEnum target, const uint cellSize = 4096) pure {
+//	import std.conv : to;
+	string result = `((const ubyte[] input){
+	uint iPos, oPos;
+	ubyte[] output; 
+	ubyte[` ~ cellSize.itos ~ `] cells; 
+	ubyte* thisPtr = cells.ptr;
+	output.length = 1024;` ~ "\n\n";
+	
 	uint iLvl = 1;
 
 
@@ -81,7 +88,7 @@ string genCode(const RepeatedToken[] programm, const TargetEnum target, const ui
 					foreach(_;0 .. rt.count) result ~= "*thisPtr = input[iPos++];\n".indentBy(iLvl);
 				} break;
 				case OutputVal : {
-					foreach(_;0 .. rt.count) result ~= "output ~= *thisPtr;\n".indentBy(iLvl);
+					foreach(_;0 .. rt.count) result ~= "output[oPos++] = *thisPtr;\n".indentBy(iLvl);
 				} break;
 				
 
@@ -92,7 +99,7 @@ string genCode(const RepeatedToken[] programm, const TargetEnum target, const ui
 
 		}
 
-		return result ~ "\nreturn output;})";
+		return result ~ "\nreturn output[0 .. oPos];})";
 	}
 
 	assert(0, "Target Not supported: " ~ itos(target));
